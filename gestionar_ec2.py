adaba33a7f23d85e267659ed710e54dc30930c68
Ernesto6678 <cortesernesto458@gmail.com>
2026-04-06 09:21:22 -0600

    Update gestionar_ec2.py

--- gestionar_ec2.py
+++ gestionar_ec2.py
@@ -1,26 +1,19 @@
 import boto3
 
-ec2 = boto3.resource('ec2', region_name='us-east-1')
+ec2 = boto3.client('ec2', region_name='us-east-1')
 s3 = boto3.client('s3')
 
-def listar_recursos():
-    print("--- Listando Buckets S3 ---")
+def ejecutar_reporte():
+    print("--- 1. Verificando Almacenamiento (S3) ---")
     buckets = s3.list_buckets()
-    for bucket in buckets['Buckets']:
-        print(f"Bucket: {bucket['Name']}")
+    for b in buckets['Buckets']:
+        print(f"Bucket: {b['Name']}")
 
-def crear_instancia():
-    print("\n--- Creando Instancia de Prueba (t2.micro) ---")
-    # Usamos el LabRole porque Learner Lab no deja crear roles
-    instancia = ec2.create_instances(
-        ImageId='ami-0c101f26f147fa7fd', # Amazon Linux 2023
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName='vockey' # Key por defecto en Learner Lab
-    )
-    print(f"Instancia creada con ID: {instancia[0].id}")
+    print("\n--- 2. Estado de Servidores (EC2) ---")
+    instancias = ec2.describe_instances()
+    for r in instancias['Reservations']:
+        for i in r['Instances']:
+            print(f"ID: {i['InstanceId']} - Estado: {i['State']['Name']}")
 
 if __name__ == "__main__":
-    listar_recursos()
-    # crear_instancia() # Descomenta para ejecutar la creación
+    ejecutar_reporte()
